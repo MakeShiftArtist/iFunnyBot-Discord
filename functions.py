a7e705495dea3c3b49fa3d014d8ab5bd43365323
MakeShiftArtist <47653736+MakeShiftArtist@users.noreply.github.com>
2020-09-23 16:46:21 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -6,6 +6,13 @@ import timeago, datetime
 def props(cls):
   return [i for i in cls.__dict__.keys() if i[:1] != '_']
 
+def ifunnyuser(data):
+    user = objects.User.by_nick(data, client=robot)
+    if user == None:
+        userid = objects.User(id=data, client=robot)
+        user = userid
+    return user
+
 
 def createUserEmbed(user, lastSeen=None, isActive=None):
     userData = user._object_data
